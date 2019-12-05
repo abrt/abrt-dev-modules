6ae6cb2e017dbe1fcb16279cc3dfc8dfcbe9dd88
Ernestas Kulik <ekulik@redhat.com>
2019-12-05 09:44:23 +0100

    Add preliminary build files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="abrt-module",
+    version="1.0",
+    packages=find_packages(),
+    author="Red Hat, Inc.",
+    description="“Standalone” ABRT module for Python",
+    keywords="abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt abrt",
+    url="https://github.com/abrt/abrt-dev-modules.git",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance"
+    ],
+)
